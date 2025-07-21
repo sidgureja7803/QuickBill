06f1f1578f620c3c581b6dcaef9aaee772e0794c
sidgureja7803 <siddhantgureja39@gmail.com>
2025-07-22 04:23:52 +0530

    updated the package.json file

--- server/server.js
+++ server/server.js
@@ -41,5 +41,5 @@ app.use((err, req, res, next) => {
 });
 
 // Set port and start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
